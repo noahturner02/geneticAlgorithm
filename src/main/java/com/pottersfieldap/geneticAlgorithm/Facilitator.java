70a69d1220842bf25cb33c9ec219ad76bc80799b
noahturner02 <noahturner02@gmail.com>
2023-04-11 15:47:47 -0500

    Add a facilitator schedule
    Should make the fitness function easier

--- src/main/java/com/pottersfieldap/geneticAlgorithm/Facilitator.java
+++ src/main/java/com/pottersfieldap/geneticAlgorithm/Facilitator.java
@@ -1,14 +1,26 @@
 package com.pottersfieldap.geneticAlgorithm;
+import org.javatuples.Pair;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Facilitator {
     private String name;
+    private List<Pair<Integer, Activity>> schedule = new ArrayList<>();
     public Facilitator(String name) {
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public List<Pair<Integer, Activity>> getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(List<Pair<Integer, Activity>>) {
+        this.schedule = schedule;
     }
 }
