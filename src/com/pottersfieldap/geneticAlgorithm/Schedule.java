01e40510570162c5e629aac8db8198744d0e88d6
noahturner02 <noahturner02@gmail.com>
2023-04-10 22:41:26 -0500

    Update Schedule.java
    Add an activity list to Schedule

--- src/com/pottersfieldap/geneticAlgorithm/Schedule.java
+++ src/com/pottersfieldap/geneticAlgorithm/Schedule.java
@@ -1,4 +1,9 @@
 package com.pottersfieldap.geneticAlgorithm;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Schedule {
+    List<Activity> activityList = new ArrayList<>();
 }
